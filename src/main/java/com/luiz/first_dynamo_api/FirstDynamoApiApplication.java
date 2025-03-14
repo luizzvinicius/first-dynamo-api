377e213bed6fe054f06297bd9879691ab2f3398f
Luiz Vinicius <viniciusadr244@gmail.com>
2025-03-13 23:07:19 -0300

    first commit

---
+++ src/main/java/com/luiz/first_dynamo_api/FirstDynamoApiApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.first_dynamo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDynamoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstDynamoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiz/first_dynamo_api/FirstDynamoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.first_dynamo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDynamoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
